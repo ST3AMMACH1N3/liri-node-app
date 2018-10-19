d0f84c7ae6718254c782c0e5ba6cb36fa0ca94a9
Ben Houston <BenjaminAHouston@gmail.com>
2018-10-19 15:44:10 -0400

    Wrote a function that reads from the random.txt file and execute commands from that file

--- liri.js
+++ liri.js
@@ -7,23 +7,39 @@ const spot = require('./keys').spotify;
 const bit = require('./keys').bandsintown;
 const omdb = require('./keys').omdb;
 
-function callAction() {
-    switch (process.argv[2].toLowerCase()) {
+function callAction(command = process.argv[2], item = process.argv[3]) {
+    switch (command.toLowerCase()) {
         case "concert-this":
-            findConcerts(process.argv[3]);
+            findConcerts(item);
             break;
         case "spotify-this-song":
-            spotifySong(process.argv[3]);
+            spotifySong(item);
             break;
         case "movie-this":
-            findMovie(process.argv[3]);
+            findMovie(item);
             break;
         case "do-what-it-says":
+            doWhatItSays();
             break;
     }
 }
 
+function doWhatItSays() {
+    //Read in the file, get rid of the quotes, split on the ,
+    let data = fs.readFileSync("random.txt", "utf8");
+    //Split the data from the file wherever there is a new line so you can have multiple commands in the file
+    data = data.split(/\r\n|\n|\r/);
+    //Loop through the array of commands
+    data.forEach((e) => {
+        //Get rid of the quotes and split each line into the comman and the item
+        e = e.replace(/['"]/g, "").split(",");
+        //Use the callAction function to call the correct function for the command, using the item
+        callAction(e[0], e[1]);
+    });
+}
+
 function findConcerts(artist) {
+    //Replace spaces with +'s so the api call doesn't error
     let band = artist.split(" ").join("+");
     request(`https://rest.bandsintown.com/artists/${band}/events?app_id=${bit.id}`, (error, response, body) => {
         if (error) {
@@ -50,6 +66,7 @@ function spotifySong(song = "The Sign") {
         if (error) {
             return console.log(error);
         }
+        console.log("*******************************");
         console.log(`Artist(s): ${data.tracks.items[0].artists[0].name}`);
         for(let i = 1; i < data.tracks.items[0].artists.length; i++) {
             console.log(`       ${data.tracks.items[0].artists[i].name}`)
@@ -65,6 +82,7 @@ function findMovie(movie = "Mr. Nobody") {
         if (error) {
             return console.log(error);
         }
+        console.log("*******************************");
         let obj = JSON.parse(body);
         console.log(`Title: ${obj.Title}`);
         console.log(`Year of Release: ${obj.Year}`);
