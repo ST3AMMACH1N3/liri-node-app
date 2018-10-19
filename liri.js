8843af708090f2b5bdba32f85263afc50e671cfe
Ben Houston <BenjaminAHouston@gmail.com>
2018-10-19 01:05:30 -0400

    Wrote a function that takes the name of a song and prints out information about it in the console, also fixed the findConcerts function to list ALL events, instead of the first one

--- liri.js
+++ liri.js
@@ -13,6 +13,7 @@ function callAction() {
             findConcerts(process.argv[3]);
             break;
         case "spotify-this-song":
+            spotifySong(process.argv[3]);
             break;
         case "movie-this":
             break;
@@ -24,9 +25,29 @@ function callAction() {
 function findConcerts(artist) {
     request(`https://rest.bandsintown.com/artists/${artist}/events?app_id=${bit.id}`, (error, response, body) => {
         let obj = JSON.parse(body);
-        console.log(`Venue: ${obj[0].venue.name}`);
-        console.log(`Venue Location: ${obj[0].venue.city}, ${obj[0].venue.country}`);
-        console.log("Date: " + moment(obj[0].datetime).format('MM/DD/YYYY'));
+        for(let i = 0; i < obj.length; i++) {
+            console.log("*******************************");
+            console.log(`Venue: ${obj[i].venue.name}`);
+            console.log(`Venue Location: ${obj[i].venue.city}, ${obj[i].venue.country}`);
+            console.log("Date: " + moment(obj[i].datetime).format('MM/DD/YYYY'));
+        }
+    });
+}
+
+function spotifySong(song = "The Sign") {
+    let spotify = new Spotify({
+        id: spot.id,
+        secret: spot.secret
+    });
+
+    spotify.search({type: 'track', query: song}, (err, data) => {
+        console.log(`Artist(s): ${data.tracks.items[0].artists[0].name}`);
+        for(let i = 1; i < data.tracks.items[0].artists.length; i++) {
+            console.log(`   ${data.tracks.items[0].artists[i].name}`)
+        }
+        console.log(`Song Name: ${data.tracks.items[0].name}`);
+        console.log(`Preview Link: ${data.tracks.items[0].preview_url}`);
+        console.log(`Album: ${data.tracks.items[0].album.name}`);
     });
 }
 
