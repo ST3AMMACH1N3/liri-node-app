13a1a31e9baec1b0095f2db7dbd67a0198febc84
Ben Houston <BenjaminAHouston@gmail.com>
2018-10-19 01:32:06 -0400

    Wrote a function that uses the omdb api to find a movie and return info about it. Also added some error checking, along with splitting on spaces and joining with +'s for the BandsInTown api.

--- liri.js
+++ liri.js
@@ -16,6 +16,7 @@ function callAction() {
             spotifySong(process.argv[3]);
             break;
         case "movie-this":
+            findMovie(process.argv[3]);
             break;
         case "do-what-it-says":
             break;
@@ -23,7 +24,12 @@ function callAction() {
 }
 
 function findConcerts(artist) {
-    request(`https://rest.bandsintown.com/artists/${artist}/events?app_id=${bit.id}`, (error, response, body) => {
+    let band = artist.split(" ").join("+");
+    request(`https://rest.bandsintown.com/artists/${band}/events?app_id=${bit.id}`, (error, response, body) => {
+        if (error) {
+            return console.log(error);
+        }
+
         let obj = JSON.parse(body);
         for(let i = 0; i < obj.length; i++) {
             console.log("*******************************");
@@ -40,14 +46,35 @@ function spotifySong(song = "The Sign") {
         secret: spot.secret
     });
 
-    spotify.search({type: 'track', query: song}, (err, data) => {
+    spotify.search({type: 'track', query: song}, (error, data) => {
+        if (error) {
+            return console.log(error);
+        }
         console.log(`Artist(s): ${data.tracks.items[0].artists[0].name}`);
         for(let i = 1; i < data.tracks.items[0].artists.length; i++) {
-            console.log(`   ${data.tracks.items[0].artists[i].name}`)
+            console.log(`       ${data.tracks.items[0].artists[i].name}`)
         }
         console.log(`Song Name: ${data.tracks.items[0].name}`);
         console.log(`Preview Link: ${data.tracks.items[0].preview_url}`);
         console.log(`Album: ${data.tracks.items[0].album.name}`);
+    });
+}
+
+function findMovie(movie = "Mr. Nobody") {
+    request(`http://www.omdbapi.com/?apikey=${omdb.key}&t=${movie}`, (error, response, body) => {
+        if (error) {
+            return console.log(error);
+        }
+        let obj = JSON.parse(body);
+        console.log(`Title: ${obj.Title}`);
+        console.log(`Year of Release: ${obj.Year}`);
+        for(let i = 0; i < obj.Ratings.length - 1; i++) {
+            console.log(`${obj.Ratings[i].Source} Rating: ${obj.Ratings[i].Value}`);
+        }
+        console.log(`Country(s): ${obj.Country}`);
+        console.log(`Language(s): ${obj.Language}`);
+        console.log(`Plot: ${obj.Plot}`);
+        console.log(`Actors: ${obj.Actors}`);
     });
 }
 
