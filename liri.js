9e4e604a47145312527131b6dfa7ac7ae1d692af
Ben Houston <BenjaminAHouston@gmail.com>
2018-10-19 00:28:57 -0400

    Wrote a function that uses the BandsInTown api to find info on a given artist. Also wrote a function that will decide which action function to call based on the arguments in the command line.

--- liri.js
+++ liri.js
@@ -6,3 +6,28 @@ const moment = require('moment');
 const spot = require('./keys').spotify;
 const bit = require('./keys').bandsintown;
 const omdb = require('./keys').omdb;
+
+function callAction() {
+    switch (process.argv[2].toLowerCase()) {
+        case "concert-this":
+            findConcerts(process.argv[3]);
+            break;
+        case "spotify-this-song":
+            break;
+        case "movie-this":
+            break;
+        case "do-what-it-says":
+            break;
+    }
+}
+
+function findConcerts(artist) {
+    request(`https://rest.bandsintown.com/artists/${artist}/events?app_id=${bit.id}`, (error, response, body) => {
+        let obj = JSON.parse(body);
+        console.log(`Venue: ${obj[0].venue.name}`);
+        console.log(`Venue Location: ${obj[0].venue.city}, ${obj[0].venue.country}`);
+        console.log("Date: " + moment(obj[0].datetime).format('MM/DD/YYYY'));
+    });
+}
+
+callAction();
